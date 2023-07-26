f0891817cbf06c2582163937348f48de913a087b
sujnanpatil <121504071+sujnanpatil@users.noreply.github.com>
2023-07-26 16:03:50 +0530

    Add files via upload

---
+++ vscodeinstal.ps1
@@ -0,0 +1,10 @@
+﻿#Install chocolatey
+
+$env:chocolateyUseWindowsCompression = 'true'
+$env:chocolateyIgnoreRebootDetected = 'true'
+$env:chocolateyVersion = '1.4.0'
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
+# instal vs code
+choco install vscode
